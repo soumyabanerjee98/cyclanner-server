59349affb4f409985c81fb54a91f7c81dc1334eb
Soumya Banerjee <banerjeesoumya771@gmail.com>
2026-05-15 18:32:44 +0530

    Auth Error handlers added

--- src/controller/v1/auth.controller.ts
+++ src/controller/v1/auth.controller.ts
@@ -1,13 +1,21 @@
 import { authService } from '@/service/index.js';
 import type { Request, Response } from 'express';
 
-export const register = async (req: Request, res: Response) => {
-  const result = await authService.register(req.body);
-  res.json(result);
+export const register = async (req: Request, res: Response, next: Function) => {
+  try {
+    const result = await authService.register(req.body);
+    return res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const result = await authService.login(email, password);
-  res.json(result);
+export const login = async (req: Request, res: Response, next: Function) => {
+  try {
+    const { email, password } = req.body;
+    const result = await authService.login(email, password);
+    return res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
